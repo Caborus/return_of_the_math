d2d06f3b480c87c0cb64c0a828902dbe0a6ae130
bobbleheadhobo <supersoup4@gmail.com>
2023-11-22 12:53:18 -0700

    got the basic functionality working

--- background.js
+++ background.js
@@ -1,14 +1,5 @@
 // Background script for the extension
 
-// This listens for a message from content.js
-chrome.runtime.onConnect.addListener(function (port) {
-  console.assert(port.name === 'content-script');
-  port.onMessage.addListener(function (msg) {
-    console.log('Message received in background:', msg);
-    // Note: This is probably where I need to fix the popup not working...
-  });
-});
-
 let isRedirectAllowed = false;
 let targetLinkHref = '';
 
@@ -17,11 +8,12 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   console.log('Message received in background:', request);
 
   if (request.action === 'interceptedClick') {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      console.log('Sending openPopup message to tab:', tabs[0].id);
-      chrome.tabs.sendMessage(tabs[0].id, { action: 'openPopup' });
-      targetLinkHref = request.targetLinkHref;
-    });
+    // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    //   console.log('Sending openPopup message to tab:', tabs[0].id);
+    //   chrome.tabs.sendMessage(tabs[0].id, { action: 'openPopup' });
+    //   targetLinkHref = request.targetLinkHref;
+    // });
+    chrome.tabs.executeScript(null, { file: "popup.js" });
   } else if (request.action === 'proceedWithRedirect') {
     isRedirectAllowed = true;
     console.log('Redirect is allowed.');
@@ -36,4 +28,3 @@ chrome.webNavigation.onCompleted.addListener(function (details) {
     chrome.tabs.update(details.tabId, { url: targetLinkHref });
   }
 });
-
--- content.js
+++ content.js
@@ -1,15 +1,110 @@
+// Content script for the extension
 
-// Sends a message when a link with the classes game-item and
-// mobile - not - playable is clicked. Need to figure out how
-// to get this to work for all game-item classes.
-document.addEventListener('click', function (event) {
-  const targetLink = event.target.closest('.game-item.mobile-not-playable a, .game-item.other-category-class a');
-const port = chrome.runtime.connect({ name: 'content-script' });
-port.postMessage({ action: 'interceptedClick', targetLinkHref: targetLink.href });
+// Function to generate a random math problem
+function generateRandomProblem() {
+  const operand1 = Math.floor(Math.random() * 10) + 1;
+  const operand2 = Math.floor(Math.random() * 10) + 1;
+  const operators = ['+', '-', '*'];
+  const operator = operators[Math.floor(Math.random() * operators.length)];
 
-  if (targetLink) {
-    event.preventDefault();
-    console.log('Link clicked. Sending message to background.');
-    chrome.runtime.sendMessage({ action: 'interceptedClick', targetLinkHref: targetLink.href });
+  return {
+      operand1: operand1,
+      operand2: operand2,
+      operator: operator,
+      toString: function() {
+          return `${operand1} ${operator} ${operand2}`;
+      }
+  };
+}
+
+// Function to calculate the answer of the math problem
+function calculateAnswer(problem) {
+  switch (problem.operator) {
+      case '+':
+          return problem.operand1 + problem.operand2;
+      case '-':
+          return problem.operand1 - problem.operand2;
+      case '*':
+          return problem.operand1 * problem.operand2;
+      default:
+          return NaN; // Handle unsupported operators
   }
+}
+
+// Function to create the math problem overlay
+function createMathProblemOverlay(problem) {
+  const overlay = document.createElement('div');
+  overlay.id = 'mathProblemOverlay';
+  overlay.style.position = 'fixed';
+  overlay.style.top = '0';
+  overlay.style.left = '0';
+  overlay.style.width = '100%';
+  overlay.style.height = '100%';
+  overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+  overlay.style.zIndex = '1000';
+  overlay.style.display = 'flex';
+  overlay.style.justifyContent = 'center';
+  overlay.style.alignItems = 'center';
+  
+  const container = document.createElement('div');
+  container.style.padding = '20px';
+  container.style.backgroundColor = 'black';
+  container.style.borderRadius = '5px';
+
+  const mathProblem = document.createElement('p');
+  mathProblem.textContent = `Solve: ${problem.toString()}`;
+  container.appendChild(mathProblem);
+
+  const input = document.createElement('input');
+  input.type = 'text';
+  container.appendChild(input);
+
+  const submitButton = document.createElement('button');
+  submitButton.textContent = 'Submit Answer';
+  container.appendChild(submitButton);
+
+  const errorMessage = document.createElement('p');
+  errorMessage.style.color = 'red';
+  errorMessage.textContent = '';
+  container.appendChild(errorMessage);
+
+
+  // Adding an event listener to close the overlay if clicked outside the popup container
+  overlay.addEventListener('click', function(event) {
+    if (event.target === overlay) {
+        overlay.remove(); // Remove the overlay if the click is directly on the overlay
+    }
+});
+
+// Prevent clicks inside the container from closing the overlay
+container.addEventListener('click', function(event) {
+    event.stopPropagation();
+});
+
+  overlay.appendChild(container);
+  document.body.appendChild(overlay);
+
+  return { overlay, input, submitButton, errorMessage };
+}
+
+// Add event listener to document
+document.addEventListener('click', function(event) {
+  let target = event.target.closest('.game-item a');
+  if (!target) return;
+
+  event.preventDefault();
+
+  const problem = generateRandomProblem();
+  const { overlay, input, submitButton, errorMessage } = createMathProblemOverlay(problem);
+
+  // Handle submission of answer
+  submitButton.addEventListener('click', function() {
+      if (parseInt(input.value, 10) === calculateAnswer(problem)) {
+          overlay.remove();
+          window.location.href = target.href; // Redirect to the game
+      } else {
+        errorMessage.textContent = 'Incorrect answer. Try again.';
+        input.value = ''; // Clear the input field
+      }
+  });
 });
